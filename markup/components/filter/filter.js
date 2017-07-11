a0b0e9981f10d3df1c5365203cc1f213ba8b99c0
Alexey Bogoslovskiy <bogoslovskiya@gmail.com>
2017-07-11 15:31:19 +0300

    fiter extended

--- markup/components/filter/filter.js
+++ markup/components/filter/filter.js
@@ -1,92 +1,193 @@
 const yearsOldSlider = () => {
-    const slider = $('#filter-age');
+    const slider = $('.js-filter-age');
 
-    const minInput = slider
-        .parents('.range-slider')
-        .find('.range-slider__input_min');
-
-    const maxInput = slider
-        .parents('.range-slider')
-        .find('.range-slider__input_max');
-
-    const minValue = minInput
-        .parent()
-        .find('.range-slider__value');
-
-    const maxValue = maxInput
-        .parent()
-        .find('.range-slider__value');
-
-
-    if (!slider.length || !minInput.length || !maxInput.length || !minValue.length || !maxValue.length) {
+    if (!slider.length) {
         return;
     }
 
-    const values = [18, 28];
+    slider.each(function() {
+        const el = $(this);
 
-    minInput.val(values[0]);
-    maxInput.val(values[1]);
-    minValue.text(values[0]);
-    maxValue.text(values[1]);
+        const minInput = el
+            .parents('.range-slider')
+            .find('.range-slider__input_min');
 
-    slider.slider({
-        range: true,
-        min: 18,
-        max: 35,
-        values,
-        slide(e, ui) {
-            const [min, max] = ui.values;
-            minInput.val(min);
-            maxInput.val(max);
-            minValue.text(min);
-            maxValue.text(max);
-        },
+        const maxInput = el
+            .parents('.range-slider')
+            .find('.range-slider__input_max');
+
+        const minValue = minInput
+            .parent()
+            .find('.range-slider__value');
+
+        const maxValue = maxInput
+            .parent()
+            .find('.range-slider__value');
+
+        const values = [18, 28];
+
+        minInput.val(values[0]);
+        maxInput.val(values[1]);
+        minValue.text(values[0]);
+        maxValue.text(values[1]);
+
+        el.slider({
+            range: true,
+            min: 18,
+            max: 35,
+            values,
+            slide(e, ui) {
+                const [min, max] = ui.values;
+                minInput.val(min);
+                maxInput.val(max);
+                minValue.text(min);
+                maxValue.text(max);
+            },
+        });
     });
 };
 
-const costSlider = () => {
-    const slider = $('#filter-cost');
+const rateSlider = () => {
+    const slider = $('.js-filter-rate');
 
-    const minInput = slider
-        .parents('.range-slider')
-        .find('.range-slider__input_min');
-
-    const maxInput = slider
-        .parents('.range-slider')
-        .find('.range-slider__input_max');
-
-    const minValue = minInput
-        .parent()
-        .find('.range-slider__value');
-
-    const maxValue = maxInput
-        .parent()
-        .find('.range-slider__value');
-
-
-    if (!slider.length || !minInput.length || !maxInput.length || !minValue.length || !maxValue.length) {
+    if (!slider.length) {
         return;
     }
 
     const values = [30, 1500];
 
-    minInput.val(values[0]);
-    maxInput.val(values[1]);
-    minValue.text(values[0]);
-    maxValue.text(values[1]);
+    slider.each(function() {
+        const el = $(this);
+        const minInput = el
+            .parents('.range-slider')
+            .find('.range-slider__input_min');
 
-    slider.slider({
-        range: true,
-        min: 30,
-        max: 1500,
-        values,
-        slide(e, ui) {
-            const [min, max] = ui.values;
-            minInput.val(min);
-            maxInput.val(max);
-            minValue.text(min);
-            maxValue.text(max);
-        },
+        const maxInput = el
+            .parents('.range-slider')
+            .find('.range-slider__input_max');
+
+        const minValue = minInput
+            .parent()
+            .find('.range-slider__value');
+
+        const maxValue = maxInput
+            .parent()
+            .find('.range-slider__value');
+
+        minInput.val(values[0]);
+        maxInput.val(values[1]);
+        minValue.text(values[0]);
+        maxValue.text(values[1]);
+
+        el.slider({
+            range: true,
+            min: 30,
+            max: 1500,
+            values,
+            slide(e, ui) {
+                const [min, max] = ui.values;
+                minInput.val(min);
+                maxInput.val(max);
+                minValue.text(min);
+                maxValue.text(max);
+            },
+        });
+    });
+};
+
+const heightSlider = () => {
+    const slider = $('.js-filter-height');
+
+    if (!slider.length) {
+        return;
+    }
+
+    const values = [135, 195];
+
+    slider.each(function() {
+        const el = $(this);
+        const minInput = el
+            .parents('.range-slider')
+            .find('.range-slider__input_min');
+
+        const maxInput = el
+            .parents('.range-slider')
+            .find('.range-slider__input_max');
+
+        const minValue = minInput
+            .parent()
+            .find('.range-slider__value');
+
+        const maxValue = maxInput
+            .parent()
+            .find('.range-slider__value');
+
+        minInput.val(values[0]);
+        maxInput.val(values[1]);
+        minValue.text(values[0]);
+        maxValue.text(values[1]);
+
+        el.slider({
+            range: true,
+            min: 135,
+            max: 195,
+            values,
+            slide(e, ui) {
+                const [min, max] = ui.values;
+                minInput.val(min);
+                maxInput.val(max);
+                minValue.text(min);
+                maxValue.text(max);
+            },
+        });
+    });
+};
+
+const weightSlider = () => {
+    const slider = $('.js-filter-weight');
+
+    if (!slider.length) {
+        return;
+    }
+
+    const values = [40, 200];
+
+    slider.each(function() {
+        const el = $(this);
+        const minInput = el
+            .parents('.range-slider')
+            .find('.range-slider__input_min');
+
+        const maxInput = el
+            .parents('.range-slider')
+            .find('.range-slider__input_max');
+
+        const minValue = minInput
+            .parent()
+            .find('.range-slider__value');
+
+        const maxValue = maxInput
+            .parent()
+            .find('.range-slider__value');
+
+        minInput.val(values[0]);
+        maxInput.val(values[1]);
+        minValue.text(values[0]);
+        maxValue.text(values[1]);
+
+        el.slider({
+            range: true,
+            min: 40,
+            max: 200,
+            values,
+            slide(e, ui) {
+                const [min, max] = ui.values;
+                minInput.val(min);
+                maxInput.val(max);
+                minValue.text(min);
+                maxValue.text(max);
+            },
+        });
     });
 };
 
@@ -98,5 +199,7 @@ export default function filter() {
     }
 
     yearsOldSlider();
-    costSlider();
+    rateSlider();
+    heightSlider();
+    weightSlider();
 }
