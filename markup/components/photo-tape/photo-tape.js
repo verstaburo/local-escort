624cad82a34c8ee8d5c22106876ea547428d35f1
Alexey Bogoslovskiy <bogoslovskiya@gmail.com>
2017-07-29 17:48:20 +0300

    fix popups

--- markup/components/photo-tape/photo-tape.js
+++ markup/components/photo-tape/photo-tape.js
@@ -1,5 +1,38 @@
 import Swiper from 'swiper';
 
+function togglePopup() {
+    const block = $('.photo-tape');
+
+    block.find('.photo-tape__item').off('hover');
+    block.off('click');
+
+    if (!!('ontouchstart' in window)) {
+        block.on('click', '.photo-tape__link', function(e) {
+            console.log('click');
+            e.preventDefault();
+
+            const popup = $(this).siblings('.model-info-popup');
+
+            popup.parents('.photo-tape').find('.model-info-popup').fadeOut();
+
+            if (popup.is(':visible')) {
+                popup.fadeOut();
+            } else {
+                popup.fadeIn();
+            }
+        });
+    } else {
+        block.find('.photo-tape__item').hover(
+            function() {
+                $(this).find('.model-info-popup').fadeIn();
+            },
+            function() {
+                $(this).find('.model-info-popup').fadeOut();
+            }
+        );
+    }
+}
+
 export default function photoTape() {
     const block = $('.photo-tape .swiper-container');
 
@@ -11,4 +44,6 @@ export default function photoTape() {
         slidesPerView: 'auto',
         freeMode: true
     });
+
+    togglePopup();
 }
