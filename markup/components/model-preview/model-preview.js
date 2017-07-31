d01759979d443adac64ad077bdc06aba5402ab3d
Alexey Bogoslovskiy <bogoslovskiya@gmail.com>
2017-07-31 12:00:07 +0300

    show breakinfo

--- markup/components/model-preview/model-preview.js
+++ markup/components/model-preview/model-preview.js
@@ -1,9 +1,50 @@
 import Swiper from 'swiper';
 
+function toggleBreakInfo() {
+    const infoBtn = $('.model-preview__action_info');
+
+    infoBtn.off('hover');
+    infoBtn.off('click');
+
+    // show breakinfo
+    if ('ontouchstart' in window) {
+        infoBtn.on('click', function(e) {
+            e.preventDefault();
+            const btn = $(this);
+            const isActive = btn.hasClass('active');
+            const breakInfo = btn.parents('.model-preview').find('.model-preview__breakinfo');
+
+            if (isActive) {
+                btn.removeClass('active');
+                breakInfo.fadeOut();
+            } else {
+                btn.addClass('active');
+                breakInfo.fadeIn();
+            }
+        });
+    } else {
+        infoBtn.hover(
+            function() {
+                const btn = $(this);
+                const breakInfo = btn.parents('.model-preview').find('.model-preview__breakinfo');
+                btn.addClass('active');
+                breakInfo.fadeIn();
+            },
+            function() {
+                const btn = $(this);
+                const breakInfo = btn.parents('.model-preview').find('.model-preview__breakinfo');
+                btn.removeClass('active');
+                breakInfo.fadeOut();
+            }
+        );
+    }
+}
+
 export default function modelPreview() {
 
     const settings = {
         slidesPerView: 1,
+        touchRatio: 0,
         prevButton: '.model-preview__button_prev',
         nextButton: '.model-preview__button_next',
         pagination: '.model-preview__pagination',
@@ -17,6 +58,7 @@ export default function modelPreview() {
 
     let slider = new Swiper(block, settings);
 
+    // grid
     const grid = $('.js-model-preview-grid');
 
     if (!grid.length) {
@@ -32,5 +74,6 @@ export default function modelPreview() {
 
         slider.forEach(item => item.destroy());
         slider = new Swiper(grid.find('.model-preview__content'), settings);
+        toggleBreakInfo();
     });
 }
