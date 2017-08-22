13415d3615427c75f0d59518d44fafc6a6cba789
Alexey Bogoslovskiy <bogoslovskiya@gmail.com>
2017-08-22 14:22:02 +0300

    scroll-top

--- markup/components/user-profile/user-profile.js
+++ markup/components/user-profile/user-profile.js
@@ -1,3 +1,40 @@
 export default function userProfile() {
+    const block = $('.user-profile');
 
+    if (!block.length) {
+        return;
+    }
+
+    const card = block.find('.profile-card');
+    const header = $('.header');
+
+    const hT = header.offset().top;
+    const wT = $(window).scrollTop();
+    const sT = card.parent().offset().top;
+    const cT = card.offset().top;
+
+    const next = card.next();
+    const parent = card.parent();
+
+    const top = hT >= wT ? hT + header.outerHeight() : wT;
+    const btm = next.length ? next.offset().top - next.outerHeight() - card.outerHeight() : parent.height() - card.outerHeight();
+
+    let translate = 0;
+
+    if (top >= sT) {
+        translate = top - sT;
+    }
+
+    if (top <= sT) {
+        translate = 0;
+    }
+
+    if (top >= btm) {
+        translate = btm;
+    }
+
+    card.css('transform', `translate3d(0, ${translate}px, 0`);
 }
+
+
+$(window).on('scroll', userProfile);
