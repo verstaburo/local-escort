37d624582f6ccf25b833a0f5d394b5ba708a6256
Alexey Bogoslovskiy <bogoslovskiya@gmail.com>
2017-09-05 19:05:28 +0300

    fix searchbar

--- markup/components/search-bar/search-bar.js
+++ markup/components/search-bar/search-bar.js
@@ -1,5 +1,8 @@
 export default function searchBar() {
-    const form = $('.search-bar');
+    const form = $('.search-bar').filter(function () {
+        return !$(this).parent().hasClass('header__search-bar');
+    });
+
     const input = form.find('.search-bar__input');
     const close = form.find('.search-bar__close');
 
@@ -13,7 +16,11 @@ export default function searchBar() {
             .parents('.navbar__item')
             .addClass('active')
             .prevAll(':not(.navbar__item_logo)')
-            .hide();
+            .each(function() {
+                $(this)
+                    .data('min-width', $(this).css('min-width'))
+                    .velocity({ minWidth: 0, width: 0, opacity: 0 }, 250);
+            });
     });
 
     close.on('click', (e) => {
@@ -23,6 +30,24 @@ export default function searchBar() {
             .parents('.navbar__item')
             .removeClass('active')
             .prevAll(':not(.navbar__item_logo)')
-            .show();
+            .each(function() {
+                const item = $(this);
+                item.velocity({ minWidth: item.data('min-width'), opacity: 1 });
+            })
+    });
+
+    $(window).on('resize', () => {
+        const sb = form.find('search-bar_extended');
+
+        if (!sb.length) {
+            return;
+        }
+
+        sb.parents('.navbar__item')
+            .addClass('active')
+            .prevAll(':not(.navbar__item_logo)')
+            .each(function() {
+                $(this).data('min-width', $(this).css('min-width'));
+            });
     });
 }
