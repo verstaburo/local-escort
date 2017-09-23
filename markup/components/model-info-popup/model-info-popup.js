4e3fd636ee6871a8fd5c13916951ad223aa6f2c3
Alla Kraynova <aukra25@yandex.ru>
2017-09-23 17:22:19 +0300

    fixes tansition and transforms on popup-models&city-suggested-popup

--- markup/components/model-info-popup/model-info-popup.js
+++ markup/components/model-info-popup/model-info-popup.js
@@ -1,6 +1,5 @@
 export default function modelInfoPopup() {
     $(document).on('click', '.model-info-popup', function(e) {
-        e.preventDefault();
         e.stopPropagation();
 
         if ($(this).hasClass('close')) {
