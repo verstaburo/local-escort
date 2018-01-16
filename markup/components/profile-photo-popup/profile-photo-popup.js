f8ab396024e895765606bb8b7ff4023c0ab36c36
Алексей Богословский <bogoslovskiya@gmail.com>
2018-01-16 20:48:02 +0300

    fixes

--- markup/components/profile-photo-popup/profile-photo-popup.js
+++ markup/components/profile-photo-popup/profile-photo-popup.js
@@ -1,61 +1,58 @@
 import Swiper from 'swiper';
 
 export default function profilePhotoPopup() {
-    const block = $('.profile-photo-popup');
+    $(document).on('show', '#profile-photo', function () {
+        const popup = $(this);
+        const block = popup.find('.profile-photo-popup');
+        const gallery = block.find('.profile-photo-popup__gallery');
+        const thumbs = block.find('.profile-photo-popup__thumbs');
 
-    if (!block.length) {
-        return false;
-    }
+        const galleryCfg = {
+            slidesPerView: 1,
+            initialSlide: 0,
+            nextButton: '.profile-photo-popup__button_next',
+            prevButton: '.profile-photo-popup__button_prev',
+            onSlideChangeStart({container}) {
+                const $container = $(container);
+                const activeSlide = $container.find('.swiper-slide-active');
+                const isLiked = activeSlide.data('liked') === 'true';
 
-    const popup = block.parents('.popup');
-    const gallery = block.find('.profile-photo-popup__gallery');
-    const thumbs = block.find('.profile-photo-popup__thumbs');
+                const likesEl = $container
+                    .parents('.profile-photo-popup__container')
+                    .find('.js-like-photo');
 
-    const galleryCfg = {
-        slidesPerView: 1,
-        initialSlide: 0,
-        nextButton: '.profile-photo-popup__button_next',
-        prevButton: '.profile-photo-popup__button_prev',
-        onSlideChangeStart({ container }) {
-            const $container = $(container);
-            const activeSlide = $container.find('.swiper-slide-active');
-            const isLiked = activeSlide.data('liked') === 'true';
+                if (isLiked) {
+                    likesEl.addClass('active');
+                } else {
+                    likesEl.removeClass('active');
+                }
 
-            const likesEl = $container
-                .parents('.profile-photo-popup__container')
-                .find('.js-like-photo');
-
-            if (isLiked) {
-                likesEl.addClass('active');
-            } else {
-                likesEl.removeClass('active');
+                likesEl
+                    .find('.js-likes-count')
+                    .text(activeSlide.data('likes') || '');
             }
+        };
 
-            likesEl
-                .find('.js-likes-count')
-                .text(activeSlide.data('likes') || '');
-        }
-    };
+        let gallerySlider = new Swiper(gallery, galleryCfg);
 
-    let gallerySlider = new Swiper(gallery, galleryCfg);
+        const thumbsCfg = {
+            slidesPerView: 'auto',
+            initialSlide: 0,
+            centeredSlides: true,
+            touchRatio: 0.2,
+            slideToClickedSlide: true
+        };
 
-    const thumbsCfg = {
-        slidesPerView: 'auto',
-        initialSlide: 0,
-        centeredSlides: true,
-        touchRatio: 0.2,
-        slideToClickedSlide: true
-    };
+        let thumbsSlider = new Swiper(thumbs, thumbsCfg);
 
-    let thumbsSlider = new Swiper(thumbs, thumbsCfg);
+        gallerySlider.params.control = thumbsSlider;
+        thumbsSlider.params.control = gallerySlider;
+    });
 
-    gallerySlider.params.control = thumbsSlider;
-    thumbsSlider.params.control = gallerySlider;
-
-    popup.on('aftershow', function() {
+    $(document).on('aftershow', '#profile-photo', function () {
         $(this)
             .find('.swiper-container')
-            .each(function() {
+            .each(function () {
                 const el = $(this);
                 const slider = el[0].swiper;
 
@@ -70,12 +67,12 @@ export default function profilePhotoPopup() {
     });
 
     // likes
-    $(document).on('click', '.js-like-photo', function(e) {
+    $(document).on('click', '.js-like-photo', function (e) {
         e.preventDefault();
 
         const self = $(this);
         const likesEl = self.find('.js-likes-count');
-        const activeSlide = $(gallerySlider.container).find('.swiper-slide-active');
+        const activeSlide = $('.profile-photo-popup__gallery').find('.swiper-slide-active');
 
         let likesCount = Number(likesEl.text()) || 0;
 
@@ -89,7 +86,9 @@ export default function profilePhotoPopup() {
             activeSlide.data('liked', 'true');
         }
 
-        const nextLikes = likesCount <= 0 ? '' : likesCount;
+        const nextLikes = likesCount <= 0
+            ? ''
+            : likesCount;
 
         likesEl.text(nextLikes);
         activeSlide.data('likes', nextLikes);
